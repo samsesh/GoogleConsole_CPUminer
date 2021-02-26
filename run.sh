81baf9b52abf8eda7bb64fbb44ce0baa48575f62
127.0.0.1 <22117518+samsesh@users.noreply.github.com>
2021-02-26 23:53:52 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+figlet run miner
+echo "BTC donation address: BC1QF3NTHFRDKD7QJN4KP6YJDRRFSX5WHU39P9X5CP "
 cd cpuminer-multi
 sudo ./cpuminer -o stratum+tcp://mmpool.org:3333 -u samsesh -p anything
