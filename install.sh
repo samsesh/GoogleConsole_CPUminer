42c3714f9f8ed8578a26eaa9f26bc782dd57dbeb
127.0.0.1 <22117518+samsesh@users.noreply.github.com>
2021-02-26 23:52:16 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 sudo apt install figlet -y
 clear
 figlet -f banner start install 
+echo "BTC donation address: BC1QF3NTHFRDKD7QJN4KP6YJDRRFSX5WHU39P9X5CP "
 sleep 3
 figlet update repo
 sleep 2
@@ -9,15 +10,18 @@ sudo apt update
 sleep 1
 clear
 figlet upgrade repo
+echo "BTC donation address: BC1QF3NTHFRDKD7QJN4KP6YJDRRFSX5WHU39P9X5CP "
 sleep 2
 sudo apt upgrade -y
 sleep 1
 clear
 figlet install req
+echo "BTC donation address: BC1QF3NTHFRDKD7QJN4KP6YJDRRFSX5WHU39P9X5CP "
 sudo apt install -y automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ git curl
 clear
 sleep 02
 figlet git CUPminer
+echo "BTC donation address: BC1QF3NTHFRDKD7QJN4KP6YJDRRFSX5WHU39P9X5CP "
 rm -rf cpuminer-multi
 git clone https://github.com/tpruvot/cpuminer-multi.git
 clear
@@ -25,10 +29,12 @@ figlet done
 sleep 03
 clear
 figlet run config
+echo "BTC donation address: BC1QF3NTHFRDKD7QJN4KP6YJDRRFSX5WHU39P9X5CP "
 cd cpuminer-multi
 sudo ./build.sh
 clear
 figlet done
+echo "BTC donation address: BC1QF3NTHFRDKD7QJN4KP6YJDRRFSX5WHU39P9X5CP "
 sleep 03
 clear
 figlet pers
@@ -37,4 +43,5 @@ figlet to cansel run run.sh
 sleep 05
 clear
 figlet start miner 
+echo "BTC donation address: BC1QF3NTHFRDKD7QJN4KP6YJDRRFSX5WHU39P9X5CP "
 ./run.sh
